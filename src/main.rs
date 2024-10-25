d0f4ec8776492637344da2afbd8f437922b948f1
Arthur Baudoin <arthur.b@my-serious-game.com>
2024-10-25 09:35:56 +0200

    feat(api): :sprakles: api healthcheck route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::IntoResponse, routing::get, Json, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/healthcheck", get(health_check_handler));
+
+    println!("Server started at http://localhost:8080");
+
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap()
+}
+
+async fn health_check_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "API";
+
+    let json_response = serde_json::json!({
+        "message": MESSAGE,
+        "status": "OK"
+    });
+
+    return Json(json_response);
 }
